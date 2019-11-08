ac2761382a49b88fceddd71ab21489a2e8bd2b5b
gnuninu <dimarnicola@gmail.com>
2019-11-08 13:29:30 +0000

    Update leap15.tf

--- leap15.tf
+++ leap15.tf
@@ -17,7 +17,7 @@ data "template_file" "user_data" {
 
 # for more info about paramater check this out
 # https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/website/docs/r/cloudinit.html.markdown
-# Use CloudInit to add our ssh-key to the instance
+# Use CloudInit to add root password
 # you can add also meta_data field
 resource "libvirt_cloudinit_disk" "commoninit_sles" {
   name           = "commoninit_sles.iso"
